e71c518669f2587eacb137d9b3d2d1762659cde2
Takeuh Duplex <takeuhduplex2006@gmail.com>
2026-02-09 03:43:16 +0100

    Change API endpoint for file upload

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ function App() {
   const sendFileToAPI = async()=>{
     try {
       setIsLoading(true);
-      const response = await axios.post("https://8000-01kfy0asv24j2kkd0xxs48725n.cloudspaces.litng.ai/uploadfile", {file: file}, {
+      const response = await axios.post("https://jovialsoh-inpaintingandocrapi.hf.space/text-erase", {image: file}, {
         responseType: "blob",
         headers: {
           "Content-Type" : "multipart/form-data"
